0e48c168bfca67241614eab9423aedfee71f2f09
Unknown <avkuznetsov54@gmail.com>
2019-03-29 00:35:51 +0700

    finished creating app routes

---
+++ travel/routes/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.contrib import messages
+
+from trains.models import Train
+from .forms import *
+
+# Create your views here.
+
+
+def home(request):
+    form = RouteForm()
+    return render(request, 'routes/home.html', {'form': form})
+
+
+def find_routes(request):
+    if request.method == 'POST':
+        form = RouteForm(request.POST or None)
+        if form.is_valid():
+            data = form.changed_data
+            assert False
+        return render(request, 'routes/home.html', {'form': form})
+    else:
+        messages.error(request, 'Создайте маршрут')
+        form = RouteForm()
+        return render(request, 'routes/home.html', {'form': form})
