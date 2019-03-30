03cc19dc5e9a6dcc820dd9c104c90cffcd9caca1
Unknown <avkuznetsov54@gmail.com>
2019-03-30 13:46:42 +0700

    ignote venv

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/aleksandrkuznecov/Desktop/my_python/lessons/Django #04 (Create site on Django 2.1)/venv/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
